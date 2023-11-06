cd20670cd54ecbe10e0c1f20df9f23ec510c239b
jonathanbello123 <jonathanbellogabriel@gmail.com>
2023-11-06 19:31:13 -0300

    8commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export const Footer = () => {
     return (
         <footer className=" bg-slate-600 mt-28 ">
-            <div className=" m-3 flex justify-center gap-10 text-white text-md">
+            <div className=" m-3 flex flex-col sm:flex-row items-center justify-center sm:gap-10 gap-2 text-white text-md">
                 <a href="#">Soporte</a>
                 <a href="#">Terminos de uso</a>
                 <a href="#">Trabaja con nosotros</a>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,11 +6,11 @@ export const Contact = () => {
 
             <form className="p-[40px] m-auto flex flex-col justify-center items-center border-2 rounded-3xl">
                 <label htmlFor="nombre" className="text-white text-xl mb-5" >Nombre:</label>
-                <input type="text" name="nombre" className="w-2/4 p-3 mb-5 text-md" /> <br />
+                <input type="text" name="nombre" className=" sm:w-3/4 w-3/4 p-3 mb-5 text-md" /> <br />
                 <label htmlFor="email" className="text-white text-xl mb-5">Email:</label>
-                <input type="email" name="email" className="w-2/4 p-3 mb-5 text-md" />
+                <input type="email" name="email" className="sm:w-3/4 w-3/4 p-3 mb-5 text-md" />
                 <label className="text-white text-xl mb-5" htmlFor="">Observaciones:</label>
-                <textarea name="" id="" cols="15" rows="2" className="w-3/4 p-3 mb-10 text-md" ></textarea>
+                <textarea name="" id="" cols="15" rows="2" className="w-3/4 p-5 mb-10 text-md" ></textarea>
             </form>
 
         </div>
