3041ff085ebbc52b0de00658bdbf759ca1dfdc30
jonathanbello123 <jonathanbellogabriel@gmail.com>
2024-01-10 12:06:41 -0300

    xd

--- src/components/Categories/CategoriesContainer/CategorieWidget.jsx
+++ src/components/Categories/CategoriesContainer/CategorieWidget.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Categories } from "../categtoriesDiv/categories";
+import { Categories } from "../CategtoriesDiv/Categories";
 import { useSelector } from "react-redux";
 
 export const CategorieWidget = () => {
-    const categories = useSelector(state => state.categories.categories)
+    const categories = useSelector(state => state.categories.categories);
     return (
-        <div className=" flex flex-col gap-16 mb-20">
-            <h2 className="text-center text-white text-5xl" >Categorias</h2>
+        <div className="flex flex-col gap-16 mb-20">
+            <h2 className="text-center text-white text-5xl">Categorias</h2>
             <div className="flex justify-center gap-20">
-
                 {categories.map((category) => (
                     <Categories key={category.id} {...category} />
                 ))}
             </div>
         </div>
-
     );
 };
